d70ae61f0c9d860558a737021ff0d41400d4dd72
Filis Coba <filiscoba1@gmail.com>
2018-09-30 00:35:01 -0400

    created a 3d globe plot and a leaflet plot on the same page

---
+++ Date-a-Park.js
@@ -0,0 +1,47 @@
+function initialize() {
+    var options = {atmosphere: true, center: [42, -73], zoom: 0};
+    var earth = new WE.map('earth_div', options);
+    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+      minZoom: 0,
+      maxZoom: 5,
+      attribution: 'NASA'
+    }).addTo(earth);
+  }
+
+  var NPMap = {
+    div: 'map',
+    overlays: [{
+      filter: function (feature) {
+        return feature.properties.park === 'Yellowstone';
+      },
+      popup: {
+        title: '{{name}}'
+      },
+      styles: {
+        point: {
+          'marker-symbol': 'star'
+        }
+      },
+      type: 'geojson',
+      url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/gateway-points-of-interest.geojson'
+    }, {
+      popup: {
+        description: 'The alpha code is {{Code}}',
+        title: '{{Name}}'
+      },
+      styles: {
+        point: {
+          'marker-color': '#609321',
+          'marker-symbol': 'park'
+        }
+      },
+      type: 'geojson',
+      url: 'https://www.nps.gov/lib/npmap.js/4.0.0/examples/data/national-parks.geojson'
+    }]
+  };
+  
+  // (function () {
+  //   var s = document.createElement('script');
+  //   s.src = 'https://www.nps.gov/lib/npmap.js/4.0.0/npmap-bootstrap.js';
+  //   document.body.appendChild(s);
+  // })();
